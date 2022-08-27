8274c0f936b45d54d74f1f2b9cdf0301b1fa5de2
Eikichiiii <oooeikichiooo@gmail.com>
2022-08-27 13:50:37 +0200

    Image popup insérée dans le fichier source afin d'être accessible par tous

--- src/main/java/Fonction/popupimage.java
+++ src/main/java/Fonction/popupimage.java
@@ -9,7 +9,7 @@ package Fonction;
 
 	public static void main(String[] args) {
 		
-	ImageIcon congrats = new ImageIcon("C:\\Users\\p-tit\\eclipse-workspace\\OPENCLASSROOMCALCULATRICEFINAL\\yourock.jpg");
+	ImageIcon congrats = new ImageIcon("src/main/images/yourock.jpg");
 	JOptionPane.showInternalMessageDialog(null, congrats, "                                                                              Félicitation vous avez trouvé le nombre mystère!!!",1);
 
 	
